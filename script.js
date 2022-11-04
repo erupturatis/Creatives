29d4a44b10d7e5525a06def1c5908641729675e6
erupturatis <bb.octav@gmail.com>
2022-11-04 21:12:29 +0200

    image particles completer

--- script.js
+++ script.js
@@ -18,7 +18,13 @@ window.addEventListener("load", function () {
       this.size = this.effect.gap;
       this.vx = 0;
       this.vy = 0;
+      this.friction = 0.9;
       this.ease = 0.1;
+      this.dx = 0;
+      this.dy = 0;
+      this.distance = 0;
+      this.force = 0;
+      this.angle = 0;
     }
     draw(context) {
       context.fillStyle = this.color;
@@ -27,8 +33,19 @@ window.addEventListener("load", function () {
     update() {
       this.dx = this.effect.mouse.x - this.x;
       this.dy = this.effect.mouse.y - this.y;
-      this.x += (this.originX - this.x) * this.ease;
-      this.y += (this.originY - this.y) * this.ease;
+      this.distance = this.dx * this.dx + this.dy * this.dy;
+      this.force = -this.effect.mouse.radius / this.distance;
+
+      if (this.distance < this.effect.mouse.radius) {
+        this.angle = Math.atan2(this.dy, this.dx);
+        this.vx += this.force * Math.cos(this.angle);
+        this.vy += this.force * Math.sin(this.angle);
+      }
+
+      this.x +=
+        (this.vx *= this.friction) + (this.originX - this.x) * this.ease;
+      this.y +=
+        (this.vy *= this.friction) + (this.originY - this.y) * this.ease;
     }
     warp(dispersion) {
       this.x = Math.random() * this.effect.width;
